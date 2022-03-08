83bc970196079cb3dc11c143fdc1d3dd2388788b
rillout <fedkv@gmail.com>
2022-03-08 15:55:13 +0300

    Add Task_17

---
+++ Task_17/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе выводит перевёрнутое число.
+
+using System;
+class Program  {
+
+   static void Main()
+
+    {
+        int[] mass = new int [2];
+
+        mass [0] = Convert.ToInt32(Console.ReadLine());
+        mass [1] = Convert.ToInt32(Console.ReadLine());
+        PointCheck(mass [0], mass [1]);
+
+        void PointCheck (int x, int y)
+        {
+            if (x > 0 && y > 0)
+            {
+                Console.WriteLine("Точка лежит в первой четверти");
+            }
+            else if (x < 0 && y > 0)
+            {
+                Console.WriteLine("Точка лежит во второй четверти");
+            }
+            else if (x < 0 && y < 0)
+            {
+                Console.WriteLine("Точка лежит в третьей четверти");
+            }
+            else if (x > 0 && y < 0)
+            {
+                Console.WriteLine("Точка лежит в четвертой четверти");
+            }
+            else Console.WriteLine("Неверно заданы x и/или y");
+        }
+    
+    
+    }
+    
+
+}
