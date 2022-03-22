83b0ccddc62ca626cb2770f87b55fb5e2fb75d35
rillout <fedkv@gmail.com>
2022-03-22 23:48:27 +0300

    Add DZ 25,27

---
+++ DZ_Task_25/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два натуральных числа (A и B) и возводит число A в степень B.
+
+using System;
+//using System.Globalization;
+
+namespace DZ_Task_25
+{
+    class Program
+    {
+        static void Main()
+
+        {
+            Console.WriteLine("Введите А");
+            int nmb = Convert.ToInt32(Console.ReadLine());
+
+            int count = nmb;
+            Console.WriteLine("Введите В");
+            int degree = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i < degree; i++)
+            {
+                
+                count *= nmb;
+            }
+            Console.WriteLine(count);
+
+        }
+
+
+    }
+
+
+}
+
+
---
+++ DZ_Task_27/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+using System;
+//using System.Globalization;
+
+namespace DZ_Task_27
+{
+    class Program
+    {
+        static void Main()
+
+        {
+            Console.WriteLine("Введите число");
+            string number = Console.ReadLine();
+            //string number = "6";
+
+            SumNmb(number);
+
+            void SumNmb (string nmb)
+            {
+                int count = 0;
+                
+                for (int i = 0; i < nmb.Length; i++)
+                {
+                    int dgt = int.Parse(number[i].ToString());
+                    count += dgt;
+                }
+                Console.WriteLine(count);
+            }
+
+        }
+
+
+    }
+
+
+}
+
+
+
