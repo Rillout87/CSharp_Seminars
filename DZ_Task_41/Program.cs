89eaef80677930fe45d80617ffd1a5695e88e878
rillout <fedkv@gmail.com>
2022-03-30 21:53:01 +0300

    Add DZ_Task_41 & DZ_Task_43

---
+++ DZ_Task_41/Program.cs
@@ -0,0 +1,66 @@
+﻿// Задача 41. Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+using System;
+using System.Collections.Generic;
+//using System.Globalization;
+
+namespace DZ_Task_41
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            List<int> userArray = new List<int>();
+
+            fillArray(userArray);
+            PositiveNumbersCount(userArray);
+            //PrintResult (PositiveNumbersCount);
+
+        }
+        static void fillArray(List<int> arr)
+        {
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine("Введите число либо Enter, если завершили ввод");
+                    arr.Add(Convert.ToInt32(Console.ReadLine()));
+                }
+            }
+            catch { }
+
+        }
+
+        static void PositiveNumbersCount (List<int> ar, int j = 0, int quantity = 0)
+        {
+
+            if (j == ar.Count) PrintResult (quantity);
+            
+            if (j < ar.Count)
+            {
+                if (ar[j] > 0) quantity++;
+                PositiveNumbersCount (ar, ++j, quantity);
+            }
+
+        }
+
+        static void PrintResult (int cnt)
+        {
+
+            Console.WriteLine($"Количество чисел, введенных пользователем, больших 0 равно {cnt}");
+        }
+
+
+    }
+
+
+}
+
+
+
+
+
+
+
---
+++ DZ_Task_43/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 43. Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+using System;
+//using System.Globalization;
+
+namespace Sem5_Task_Star_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+
+            Console.WriteLine("Введите k1");
+            double k1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите k2");
+            double k2 = Convert.ToDouble(Console.ReadLine());;
+            Console.WriteLine("Введите b1");
+            double b1 = Convert.ToDouble(Console.ReadLine());;
+            Console.WriteLine("Введите b2");
+            double b2 = Convert.ToDouble(Console.ReadLine());;
+
+
+            if (b1 != b2)
+            {
+
+                double crossPointX = (b2 - b1) / (k1 - k2);
+                double crossPointY = k1 * crossPointX + b1;
+
+                Console.WriteLine(crossPointX + " , " + crossPointY);
+            }
+            else
+            {
+                Console.WriteLine("Прямые не пересекаются или полностью совпадают");
+            }
+        }
+
+
+    }
+
+
+}
+
+
+
+
+
+
+
